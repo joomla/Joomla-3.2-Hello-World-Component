6cf4bef9caabdd49cfb25da50a1df698b510e8c6
Matt Thomas <matt@betweenbrain.com>
2013-12-19 12:00:58 -0500

    Completion of second step from http://docs.joomla.org/J2.5:Developing_a_MVC_Component/Adding_a_view_to_the_site_part - Implements use of JControllerLegacy and JViewLegacy

---
+++ site/controller.php
@@ -0,0 +1,13 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+// import Joomla controller library
+jimport('joomla.application.component.controller');
+
+/**
+ * Hello World Component Controller
+ */
+class HelloWorldController extends JControllerLegacy
+{
+}

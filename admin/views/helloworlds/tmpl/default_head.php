02ab268b7298e0ac22ea97bb4d61d7b11bf380bf
Matt Thomas <matt@betweenbrain.com>
2013-12-20 12:05:17 -0500

    - Implements Joomla 3 check all JavaScript

--- admin/views/helloworlds/tmpl/default_head.php
+++ admin/views/helloworlds/tmpl/default_head.php
@@ -7,7 +7,7 @@ defined('_JEXEC') or die('Restricted Access');
 		<?php echo JText::_('COM_HELLOWORLD_HELLOWORLD_HEADING_ID'); ?>
 	</th>
 	<th width="20">
-		<input type="checkbox" name="toggle" value="" onclick="checkAll(<?php echo count($this->items); ?>);" />
+		<?php echo JHtml::_('grid.checkall'); ?>
 	</th>
 	<th>
 		<?php echo JText::_('COM_HELLOWORLD_HELLOWORLD_HEADING_GREETING'); ?>

2837ad868263dc2fd5fc95ca304630ef62e6dc16
Gunjan Patel <gunjan@redcomponent.com>
2014-02-05 15:19:46 +0530

    [add] Override function in controller

--- admin/controllers/helloworlds.php
+++ admin/controllers/helloworlds.php
@@ -16,4 +16,21 @@ defined('_JEXEC') or die;
  */
 class HelloWorldControllerHelloWorlds extends JControllerAdmin
 {
+	/**
+	 * Proxy for getModel.
+	 *
+	 * @param   string  $name    The model name. Optional.
+	 * @param   string  $prefix  The class prefix. Optional.
+	 * @param   array   $config  Configuration array for model. Optional.
+	 *
+	 * @return  object  The model.
+	 *
+	 * @since   1.6
+	 */
+	public function getModel($name = 'HelloWorld', $prefix = 'HelloWorldModel', $config = array('ignore_request' => true))
+	{
+		$model = parent::getModel($name, $prefix, $config);
+
+		return $model;
+	}
 }

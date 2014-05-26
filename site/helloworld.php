506b90097773177a1cf175c95360f9999c16ad55
sovainfo <webmaster@sovainfo.nl>
2014-05-26 11:57:01 +0200

    Changed according to com_content
    - Removed useless comments, they say the same thing as the statements themselves!
    - Only use variables when used more than once

--- site/helloworld.php
+++ site/helloworld.php
@@ -7,15 +7,8 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-// No direct access to this file
 defined('_JEXEC') or die;
 
-// Get an instance of the controller prefixed by HelloWorld
 $controller = JControllerLegacy::getInstance('HelloWorld');
-
-// Perform the Request task
-$input = JFactory::getApplication()->input;
-$controller->execute($input->getCmd('task'));
-
-// Redirect if set by the controller
+$controller->execute(JFactory::getApplication()->input->getCmd('task'));
 $controller->redirect();

65cf07f573a41e0e51c13ad03e2a57b6bb7ca681
Alex <scionescire@gmail.com>
2015-02-03 11:46:21 +0100

    Updated guide

--- admin/sql/updates/mysql/0.0.6.sql
+++ admin/sql/updates/mysql/0.0.6.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS `#__helloworld`;
- 
+
 CREATE TABLE `#__helloworld` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `greeting` varchar(25) NOT NULL,
    PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
- 
+
 INSERT INTO `#__helloworld` (`greeting`) VALUES
-        ('Hello World!'),
-        ('Good bye World!');
+	('Hello World!'),
+	('Good bye World!');
--- site/models/helloworld.php
+++ site/models/helloworld.php
@@ -18,17 +18,16 @@ class HelloWorldModelHelloWorld extends JModelItem
 	/**
 	 * Returns a reference to the a Table object, always creating it.
 	 *
-	 * @param       type    The table type to instantiate
-	 * @param       string  A prefix for the table class name. Optional.
-	 * @param       array   Configuration array for model. Optional.
-	 * @return      JTable  A database object
-	 * @since       2.5
+	 * @param	type	The table type to instantiate
+	 * @param	string	A prefix for the table class name. Optional.
+	 * @param	array	Configuration array for model. Optional.
+	 * @return	JTable	A database object
+	 * @since	2.5
 	 */
 	public function getTable($type = 'HelloWorld', $prefix = 'HelloWorldTable', $config = array())
 	{
 		return JTable::getInstance($type, $prefix, $config);
 	}
-
 	/**
 	 * Get the message
 	 * @param  int    The corresponding id of the message to be retrieved
@@ -45,7 +44,7 @@ class HelloWorldModelHelloWorld extends JModelItem
 		{
 			//request the selected id
 			$jinput = JFactory::getApplication()->input;
-			$id     = $jinput->get('id', 1, 'INT');
+			$id = $jinput->get('id', 1, 'INT' );
 
 			// Get a TableHelloWorld instance
 			$table = $this->getTable();

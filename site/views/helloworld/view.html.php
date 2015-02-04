80062cf23d98ce2778ee632d9baed6d84f36266b
Alex <scionescire@gmail.com>
2015-02-04 14:25:46 +0100

    Small style fix

--- site/views/helloworld/view.html.php
+++ site/views/helloworld/view.html.php
@@ -18,12 +18,12 @@ defined('_JEXEC') or die('Restricted access');
 class HelloWorldViewHelloWorld extends JViewLegacy
 {
 	/**
-	* Display the Hello World view
-	*
-	* @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
-	*
-	* @return  void
-	*/
+	 * Display the Hello World view
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  void
+	 */
 	public function display($tpl = null)
 	{
 		// Assign data to the view

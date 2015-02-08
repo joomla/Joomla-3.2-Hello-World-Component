7e94aeb8d24f4f41bd4eb21e91d045b5f79f7b2a
Alex <scionescire@gmail.com>
2015-02-08 22:22:10 +0100

    Hello world meets coding standard
    Updated guide using step 10 fix as starting point

--- site/controller.php
+++ site/controller.php
@@ -1,11 +1,11 @@
 <?php
 /**
-* @package Joomla.Administrator
-* @subpackage com_helloworld
-*
-* @copyright Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
-* @license GNU General Public License version 2 or later; see LICENSE.txt
-*/
+ * @package     Joomla.Administrator
+ * @subpackage  com_helloworld
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
@@ -13,7 +13,7 @@ defined('_JEXEC') or die('Restricted access');
 /**
  * Hello World Component Controller
  *
- * @since   0.0.1
+ * @since  0.0.1
  */
 class HelloWorldController extends JControllerLegacy
 {

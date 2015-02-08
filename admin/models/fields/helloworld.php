8d44df526ac3ac6172c7567ea0ffa2d542e05aaa
Alex <scionescire@gmail.com>
2015-02-08 22:23:02 +0100

    Hello world meets coding standard
    Updated guide using step 10 fix as starting point

--- admin/models/fields/helloworld.php
+++ admin/models/fields/helloworld.php
@@ -1,37 +1,46 @@
 <?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_helloworld
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
 
-// import the list field type
-jimport('joomla.form.helper');
 JFormHelper::loadFieldClass('list');
 
 /**
  * HelloWorld Form Field class for the HelloWorld component
+ *
+ * @since  0.0.1
  */
 class JFormFieldHelloWorld extends JFormFieldList
 {
 	/**
 	 * The field type.
 	 *
-	 * @var		string
+	 * @var         string
 	 */
 	protected $type = 'HelloWorld';
 
 	/**
 	 * Method to get a list of options for a list input.
 	 *
-	 * @return	array		An array of JHtml options.
+	 * @return  array  An array of JHtml options.
 	 */
 	protected function getOptions()
 	{
-		$db = JFactory::getDBO();
+		$db    = JFactory::getDBO();
 		$query = $db->getQuery(true);
 		$query->select('id,greeting');
 		$query->from('#__helloworld');
-		$db->setQuery((string)$query);
+		$db->setQuery((string) $query);
 		$messages = $db->loadObjectList();
-		$options = array();
+		$options  = array();
+
 		if ($messages)
 		{
 			foreach ($messages as $message)
@@ -39,7 +48,9 @@ class JFormFieldHelloWorld extends JFormFieldList
 				$options[] = JHtml::_('select.option', $message->id, $message->greeting);
 			}
 		}
+
 		$options = array_merge(parent::getOptions(), $options);
+
 		return $options;
 	}
 }

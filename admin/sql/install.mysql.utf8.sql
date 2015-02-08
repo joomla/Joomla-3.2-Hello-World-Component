3fd63572e703a2412cc2b7156d9fd4ccb2b6035c
Alex <scionescire@gmail.com>
2015-02-08 22:22:37 +0100

    Hello world meets coding standard
    Updated guide using step 10 fix as starting point

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -1,11 +1,15 @@
 DROP TABLE IF EXISTS `#__helloworld`;
 
 CREATE TABLE `#__helloworld` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `greeting` varchar(25) NOT NULL,
-   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+	`id`       INT(11)     NOT NULL AUTO_INCREMENT,
+	`greeting` VARCHAR(25) NOT NULL,
+	`published` tinyint(4) NOT NULL,
+	PRIMARY KEY (`id`)
+)
+	ENGINE =MyISAM
+	AUTO_INCREMENT =0
+	DEFAULT CHARSET =utf8;
 
 INSERT INTO `#__helloworld` (`greeting`) VALUES
-	('Hello World!'),
-	('Good bye World!');
+('Hello World!'),
+('Good bye World!');

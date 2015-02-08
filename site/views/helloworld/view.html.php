3133165725d5b1f63e408c6b67f1cfe300978242
Alex <scionescire@gmail.com>
2015-02-08 22:19:50 +0100

    Hello world meets coding standard
    Updated guide using step 10 fix as starting point

--- site/views/helloworld/view.html.php
+++ site/views/helloworld/view.html.php
@@ -1,20 +1,20 @@
 <?php
 /**
-* @package Joomla.Administrator
-* @subpackage com_helloworld
-*
-* @copyright Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
-* @license GNU General Public License version 2 or later; see LICENSE.txt
-*/
+ * @package     Joomla.Administrator
+ * @subpackage  com_helloworld
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
 
 /**
-* HTML View class for the HelloWorld Component
-*
-* @since 0.0.1
-*/
+ * HTML View class for the HelloWorld Component
+ *
+ * @since  0.0.1
+ */
 class HelloWorldViewHelloWorld extends JViewLegacy
 {
 	/**
@@ -24,7 +24,7 @@ class HelloWorldViewHelloWorld extends JViewLegacy
 	 *
 	 * @return  void
 	 */
-	public function display($tpl = null)
+	function display($tpl = null)
 	{
 		// Assign data to the view
 		$this->msg = 'Hello World';
